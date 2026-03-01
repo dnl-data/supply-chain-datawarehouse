43eaec87e0e6032e7ff8dab88799320867720d99
dnl-data <mrvksh777@gmail.com>
2026-03-01 17:29:05 +0000

    main part of extract.py + download kaggle files in data

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,34 @@
 import kagglehub
+import os
+import shutil
 
-path = kagglehub.dataset_download("rajhkumarr/e-commerce-and-retail-supply-chain")
+def download_kaggle_files(kaggle_path, save_path):
+    # Check if files already exist in destination
+    if os.path.exists(save_path) and os.listdir(save_path):
+        print(f"Files already exist in '{save_path}' folder. Skipping download.")
+        return
+    
+    try:
+        # Downloading the dataset
+        downloaded_path = kagglehub.dataset_download(kaggle_path)
+        # Creating destination folder if it doesn't exist
+        os.makedirs(save_path, exist_ok=True)
+        
+    except Exception as e:
+        print(f"An error occurred during download: {e}")
+        return
+        
+    else:
+        # Copying all files from downloaded path to save_path
+        for filename in os.listdir(downloaded_path):
+            source_file = os.path.join(downloaded_path, filename)
+            destination_file = os.path.join(save_path, filename)
+            shutil.copy2(source_file, destination_file)
 
-print("Path to dataset files:", path)
+        print(f"Files copied successfully to: '{save_path}'folder ")
+
+# Call the function
+download_kaggle_files(
+    "rajhkumarr/e-commerce-and-retail-supply-chain",
+    "data"
+)
