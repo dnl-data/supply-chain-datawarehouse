0a5809c625772fcd7e80d488508a834ec23265e8
dnl-data <mrvksh777@gmail.com>
2026-03-11 21:10:14 +0000

    config.py

--- src/transformation.py
+++ src/transformation.py
@@ -4,12 +4,19 @@ from config import DWH_PATH
 
 conn = duckdb.connect(DWH_PATH)
 
+conn.sql("DESCRIBE raw.suppliers").show()
+
+
+
 structure = """0. Vérification existence des tables →
  1. Validation types → 
  2. Validation nulls → 
  3. Colonnes dérivées"""
 
-def verify_table_exist ():
+#def verify_table_exist ():
+
+
+
 
 
 
