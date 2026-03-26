0cf29c420e1a82789278ba6bd9afa995f720bb9a
dnl-data <mrvksh777@gmail.com>
2026-03-26 15:57:01 +0000

    transformation.py

--- src/transformation.py
+++ src/transformation.py
@@ -5,11 +5,11 @@ from config import DWH_PATH,EXPECTED_SCHEMA
 conn = duckdb.connect(DWH_PATH)
 
 def verify_table_exist(conn):
-    raw_tables = conn.sql("SELECT * FROM {EXPECTED_SCHEMA} WHERE = 'raw').df()
+    raw_tables = conn.sql("SELECT * FROM {EXPECTED_SCHEMA} WHERE = 'raw'").df()
     for table in tables:
         if tables != raw_tables:
             wrong_table = .append()
-            print(this tables not good imported {wrong_table})
+            print(f"this tables not imported {wrong_table}")
 
 
 
